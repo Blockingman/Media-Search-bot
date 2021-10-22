f4f51b9b548b273eb761abd5df63aa42b3d77132
Niro Dexter <77662275+Niro-Dexter@users.noreply.github.com>
2021-10-22 17:40:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -272,11 +272,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-                    InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+                    InlineKeyboardButton('Updates Channel', url='https://t.me/NFLK_Updates'),
+                    InlineKeyboardButton('Main Bot', url='https://t.me/NetflixLK_bot')
                 ]
                 ]
-            await query.message.edit(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+            await query.message.edit(text="<b>Dev : <a href='https://t.me/NFLK_Updates'>NFLK</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nMain Bot : <a href='https://t.me/NetflixLK_bot'>NFLK Bot</a>\nUpdates Channel : <a href='https://t.me/NFLK_Updates'>NFLK Channel</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 
 
@@ -297,8 +297,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('⚡Updates⚡', url='https://t.me/NFLK_Updates'),
+                        InlineKeyboardButton('NFLK Bot', url='https://t.me/NetflixLK_bot')
                     ]
                     ]
                 
@@ -329,8 +329,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('⚡Updates⚡', url='https://t.me/NFLK_Updates'),
+                        InlineKeyboardButton('NFLK Bot', url='https://t.me/NetflixLK_bot')
                     ]
                     ]
                 
